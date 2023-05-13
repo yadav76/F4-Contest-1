0f90a3f3b5cd566e5253b3105d1dac520d729bca
santosh <yadavsantosh6720@gmail.com>
2023-05-13 23:33:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1 id="heading">React Calculator</h1>
-      <input id="num1 inputField" type="number" placeholder='Num 1'/>
-      <input id="num2 inputField" type="number" placeholder='Num 2'/>
+      <input id="num" className='inputField' type="number" placeholder='Num 1'/>
+      <input id="num2" className='inputField' type="number" placeholder='Num 2'/>
 
       <div className="operators">
         <button clasName="btn" id="add">+</button>
